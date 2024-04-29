bb438e02e2dc6b6492cb4b1882c18c768b4c9c1e
ericcadenas <cadenaseric13@gmail.com>
2024-04-28 19:52:10 -0500

    carrito-de-compras

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+// Product.cs
+namespace ShoppingCart.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
